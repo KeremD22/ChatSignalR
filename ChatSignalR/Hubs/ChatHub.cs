9598945caf43710c15f07d9aa5c5d70d366724ed
Kerem Dağlar <126280188+KeremD22@users.noreply.github.com>
2023-12-26 01:17:02 +0300

    Add project files.

---
+++ ChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+
+namespace ChatSignalR.Hubs
+
+
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string fromUser,string message)
+        {
+            await Clients.All.SendAsync("ReceiverMessage",fromUser, message);
+        }
+    }
+}
---
+++ ChatSignalR/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiverMessage", function (fromuser, message) {
+    var msg = fromuser + ":" + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+});
+
+connection.start();
+
+$("#btnSend").on("click", function () {
+    var fromUser = $("#txtUser").val();
+    var message = $("#txtMsg").val();
+
+    connection.invoke("SendMessage", fromUser, message);
+});
